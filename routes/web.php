043c9d58d2fb391158c8d183d7270bc1a134401b
SANGSANINNN6 <anangfauzan26@gmail.com>
2025-10-09 10:26:36 +0700

    pertemuan 5

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
-
+use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
 });
-
 Route::get('/mahasiswa', function () {
-    return 'Halo Mahasiswa';
 })->name('mahasiswa.show');
 
 Route::get('/nama/{param1}', function ($param1) {
     return 'Nama saya: '.$param1;
 });
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
 
 Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
 Route::get('/about', function () {
     return view('halaman-about');
-});
+})->name('route.about');
+
+Route::get('/home', [HomeController::class, 'index'])
+    ->name('home');
+
+Route::post('question/store', [QuestionController::class, 'store'])
+        ->name('question.store');
+
+        Route::get('dashboard',[DashboardController::class, 'index'])
+            ->name('dashboard');
